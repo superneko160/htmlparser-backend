a3049542470e5349f7a9745b4487d13ba9564e4f
sneko <supernekocat31@gmail.com>
2024-04-28 15:10:32 +0900

    feat:add arg includeEmpty(#1)

--- htmlparser/src/utils/getElements.ts
+++ htmlparser/src/utils/getElements.ts
@@ -2,16 +2,18 @@ import * as cheerio from 'cheerio'
 import type { AttributeOption } from './../types/types'
 
 /**
- * 指定した要素名の属性と値を取得する関数
+ * 指定した要素名の属性と値を取得
  * @param {string} contents HTMLコンテンツ
  * @param {string[]} elementNames 取得する要素
- * @param {AttributeOption} option 取得する属性のオプション（初期値：all（全属性取得））
+ * @param {AttributeOption} attributeNames 取得する属性（初期値：all（全属性取得））
+ * @param {boolean} includeEmpty 空のオブジェクトも含めるか否か（初期値：true（含める））
  * @return {Object} 各要素名をキーとする属性と値の配列を持つオブジェクト
  */
 export function getElementAttributes(
     contents: string,
     elementNames: string[],
-    option: AttributeOption = 'all'
+    attributeNames: AttributeOption = 'all',
+    includeEmpty: boolean = true
 ): { [key: string]: Object[] } {
 
     const $ = cheerio.load(contents)
@@ -23,12 +25,12 @@ export function getElementAttributes(
         const attributes: Object[] = []
 
         // 指定した要素名の要素を探索
-        $(elementName).each((i, el) => {
-            const attrs = (el as cheerio.Element).attribs
+        $(elementName).each((_, element) => {
+            const attrs = (element as cheerio.Element).attribs
             const elementAttrs: { [key: string]: string } = {}
 
-            // オプションに応じて属性を抽出
-            const shouldExtractAttribute = getShouldExtractAttributeFunction(option)
+            // 取得する属性に応じた条件分岐を備えた関数を取得
+            const shouldExtractAttribute = getShouldExtractAttributeFunction(attributeNames)
 
             for (const attr in attrs) {
                 if (shouldExtractAttribute(attr)) {
@@ -36,7 +38,9 @@ export function getElementAttributes(
                 }
             }
 
-            attributes.push(elementAttrs)
+            if (includeEmpty || Object.keys(elementAttrs).length > 0) {
+                attributes.push(elementAttrs)
+            }
         })
 
         data[elementName] = attributes
@@ -46,12 +50,15 @@ export function getElementAttributes(
 }
 
 /**
- * 属性を抽出するか否かを判定する関数を返す
- * @param {AttributeOption} option 取得する属性のオプション
+ * 属性を抽出するか否かを判定する関数を取得
+ * @param {AttributeOption} attributeNames 取得する属性
  * @return {(attr: string) => boolean} 属性を抽出するか否か判定する関数
  */
-function getShouldExtractAttributeFunction(option: AttributeOption): (attr: string) => boolean {
-    switch (option) {
+function getShouldExtractAttributeFunction(
+    attributeNames: AttributeOption
+): (attr: string) => boolean {
+
+    switch (attributeNames) {
       case 'all':
         return () => true
       case 'id':
